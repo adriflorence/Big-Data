94e2b5c0645b75169345e1b896d37ab9cb46dd5c
Adri Forczek <forczekadri@gmail.com>
2020-10-15 20:41:34 +0100

    feat: initial setup

---
+++ 01/01_lab.py
@@ -0,0 +1,24 @@
+# 1. Write a Python program to get the Python version you are using and print it out
+
+
+# 2. Write a Python program to print the following as shown:
+# Baa, baa, black sheep
+# 		Have you any wool?
+# 	Yes sir, yes sir
+# 			Three bags full.
+# One for my master
+# 	And one for the dame
+# One for the little boy
+# Who lives down the lane.
+
+
+# 3. Write a Python program to count the number of even and odd numbers from a series of
+# numbers
+
+
+# 4. Write a Python program that prints all the numbers from 0 to 50 except 37 and 16.
+
+
+# 5. Write a Python program to get the Fibonacci series between 0 and 100. The Fibonacci Sequence
+# is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... The next number is found by adding up
+# the two numbers before it.
